2b6684c3a247a91f14ec452a2ee5cf7a4e68f24c
aliciagyori <aliciajgyori@gmail.com>
2017-09-16 17:59:54 -0700

    Fix styled button.

--- src/shared/component/button.jsx
+++ src/shared/component/button.jsx
@@ -9,16 +9,16 @@ type Props = {
   color: string,
 }
 
-const ButtonStyle = styled.button `
+const ButtonStyled = styled.button `
   color: ${props => props.color};
   background-color: ${props => props.theme.main};
-  font-size: 1em;
-  padding: 0.25em 1em;
+  font-size: 1rem;
+  padding: .5% .5%;
   border: 2px solid palevioletred;
   border-radius: 3px;
 `
 
 const Button = ({ label, handleClick, color }: Props) =>
-  <button color={color} onClick={handleClick}>{label}</button>
+  <ButtonStyled color={color} onClick={handleClick}>{label}</ButtonStyled>
 
 export default Button
